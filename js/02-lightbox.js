f043aadee17efbb32a540e27d564d7e9a91edf15
Max Yuvkovetskiy <yuvkovetskyy.maxim@gmail.com>
2022-06-03 12:42:32 +0300

    second gallery - ready!

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const refs = {
+    gallery: document.querySelector('.gallery'),
+};
+
+refs.gallery.innerHTML = createGalleryMurkup();
+
+const createdGallery = new SimpleLightbox('.gallery a', {
+    caption: true,
+    captionType: 'attr',
+    overlay: true,
+    captionsData: 'alt',
+});
+
+
+
+
+
+function createGalleryMurkup(){
+    return galleryItems.map(({ preview, original, description }) => 
+        `<a class="gallery__item" href="${original}">
+                        <img class="gallery__image" src="${preview}" alt="${description}" />
+                    </a>`)
+        .join('')
+};
+
+
 console.log(galleryItems);
